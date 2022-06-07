720d5e7fe4cb3190f8c8a910dd2430afce0936da
SimicAleksa <aleksadsimic@gmail.com>
2022-06-07 21:02:54 +0200

    Manager's shortcuts

--- HCI_Project/commands/RoutedCommands.cs
+++ HCI_Project/commands/RoutedCommands.cs
@@ -8,6 +8,7 @@ namespace HCI_Project.commands
 {
     public static class RoutedCommands
     {
+        //clients
         public static readonly ICommand FormatHello;
         public static readonly ICommand FormatSearchTrainLine;
         public static readonly ICommand FormatSearchTrainTable;
@@ -15,8 +16,23 @@ namespace HCI_Project.commands
         public static readonly ICommand FormatHistoryTickets;
         public static readonly ICommand FormatHistoryReservations;
 
+        //managers
+        public static readonly ICommand FormatCreateTrain;
+        public static readonly ICommand FormatCreateTrainLine;
+        public static readonly ICommand FormatCreateTrainTable;
+
+        public static readonly ICommand FormatEditTrain;
+        public static readonly ICommand FormatEditTrainLine;
+        public static readonly ICommand FormatEditTrainTable;
+
+        public static readonly ICommand FormatMonthlyReports;
+        public static readonly ICommand FormatPerTableReports;
+
+        //public static readonly ICommand FormatLogOut;
+
         static RoutedCommands()
         {
+            // clients
             var inputGestures = new InputGestureCollection();
             inputGestures.Add(new MultiKey.MultiKeyGesture(new Key[] { Key.M, Key.B }, ModifierKeys.Control, "Ctrl+M, B"));
             FormatHello = new RoutedUICommand("Format Hello", "FormatHello", typeof(RoutedCommands), inputGestures);
@@ -41,7 +57,41 @@ namespace HCI_Project.commands
             inputGestures.Add(new MultiKey.MultiKeyGesture(new Key[] { Key.H, Key.R }, ModifierKeys.Control, "Ctrl+H, R"));
             FormatHistoryReservations = new RoutedUICommand("Format HistoryReservations", "FormatHistoryReservations", typeof(RoutedCommands), inputGestures);
 
+            //managers
+            //create
+            inputGestures = new InputGestureCollection();
+            inputGestures.Add(new MultiKey.MultiKeyGesture(new Key[] { Key.C, Key.T }, ModifierKeys.Control, "Ctrl+C, T"));
+            FormatCreateTrain = new RoutedUICommand("Format CreateTrain", "FormatCreateTrain", typeof(RoutedCommands), inputGestures);
 
+            inputGestures = new InputGestureCollection();
+            inputGestures.Add(new MultiKey.MultiKeyGesture(new Key[] { Key.C, Key.L }, ModifierKeys.Control, "Ctrl+C, L"));
+            FormatCreateTrainLine = new RoutedUICommand("Format CreateTrainLine", "FormatCreateTrainLine", typeof(RoutedCommands), inputGestures);
+
+            inputGestures = new InputGestureCollection();
+            inputGestures.Add(new MultiKey.MultiKeyGesture(new Key[] { Key.C, Key.A }, ModifierKeys.Control, "Ctrl+C ,A"));
+            FormatCreateTrainTable = new RoutedUICommand("Format CreateTrainTable", "FormatCreateTrainTable", typeof(RoutedCommands), inputGestures);
+
+            //edit
+            inputGestures = new InputGestureCollection();
+            inputGestures.Add(new MultiKey.MultiKeyGesture(new Key[] { Key.E, Key.T }, ModifierKeys.Control, "Ctrl+E, T"));
+            FormatEditTrain = new RoutedUICommand("Format EditTrain", "FormatEditTrain", typeof(RoutedCommands), inputGestures);
+
+            inputGestures = new InputGestureCollection();
+            inputGestures.Add(new MultiKey.MultiKeyGesture(new Key[] { Key.E, Key.L }, ModifierKeys.Control, "Ctrl+E, L"));
+            FormatEditTrainLine = new RoutedUICommand("Format EditTrainLine", "FormatEditTrainLine", typeof(RoutedCommands), inputGestures);
+
+            inputGestures = new InputGestureCollection();
+            inputGestures.Add(new MultiKey.MultiKeyGesture(new Key[] { Key.E, Key.A}, ModifierKeys.Control, "Ctrl+E, A"));
+            FormatEditTrainTable = new RoutedUICommand("Format EditTrainTable", "FormatEditTrainTable", typeof(RoutedCommands), inputGestures);
+
+            //reports
+            inputGestures = new InputGestureCollection();
+            inputGestures.Add(new MultiKey.MultiKeyGesture(new Key[] { Key.R, Key.M }, ModifierKeys.Control, "Ctrl+R, M"));
+            FormatMonthlyReports = new RoutedUICommand("Format EditTrainLine", "FormatEditTrainLine", typeof(RoutedCommands), inputGestures);
+
+            inputGestures = new InputGestureCollection();
+            inputGestures.Add(new MultiKey.MultiKeyGesture(new Key[] { Key.R, Key.T }, ModifierKeys.Control, "Ctrl+R, T"));
+            FormatPerTableReports = new RoutedUICommand("Format EditTrainTable", "FormatEditTrainTable", typeof(RoutedCommands), inputGestures);
         }
     }
 }
--- HCI_Project/managerPages/ManagerPage.xaml.cs
+++ HCI_Project/managerPages/ManagerPage.xaml.cs
@@ -15,22 +15,104 @@ namespace HCI_Project.managerPages
             InitializeComponent();
             MainFrame = mainFrame;
             Style = (Style)FindResource(typeof(Page));
+            ManagerFrame.Focus();
         }
 
-        private void LogOutClick(object sender, RoutedEventArgs e)
+        private void CreateTrain_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CreateTrain_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            ManagerFrame.Content = new NewTrainPage();
+            ManagerFrame.Focus();
+        }
+
+        private void CreateTrainLine_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CreateTrainLine_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            ManagerFrame.Content = new NewTrainLinePage();
+            ManagerFrame.Focus();
+        }
+
+        private void CreateTrainTable_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CreateTrainTable_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Train table in development");
+            ManagerFrame.Focus();
+        }
+
+        private void LogOut_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void LogOut_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
         {
             Database.LogOut();
             MainFrame.Content = new LogInPage(MainFrame);
         }
 
-        private void ShowNewTrainLine(object sender, RoutedEventArgs e)
+        private void EditTrain_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
         {
-            ManagerFrame.Content = new NewTrainLinePage();
+            e.CanExecute = true;
         }
 
-        private void ShowNewTrain(object sender, RoutedEventArgs e)
+        private void EditTrain_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
         {
-            ManagerFrame.Content = new NewTrainPage();
+            MessageBox.Show("Edit Train  in development");
+        }
+
+        private void EditTrainLine_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void EditTrainLine_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Edit Train line in development");
+        }
+
+        private void EditTrainTable_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void EditTrainTable_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show(" Edit Train table in development");
+            ManagerFrame.Focus();
+        }
+        ////////////////
+        ///
+        private void MonthlyReports_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void MonthlyReports_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show(" MonthlyReports in development");
+            ManagerFrame.Focus();
+        }
+        private void PerTableReports_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void PerTableReports_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show(" PerTableReports in development");
+            ManagerFrame.Focus();
         }
     }
 }
