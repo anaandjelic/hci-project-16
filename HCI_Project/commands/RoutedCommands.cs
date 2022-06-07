34bc692eb553e953a3d2b8641d60e783ee170117
SimicAleksa <aleksadsimic@gmail.com>
2022-06-07 10:12:49 +0200

    Commands basic test

---
+++ HCI_Project/commands/RoutedCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace HCI_Project.commands
+{
+    public class RoutedCommands
+    {
+        public readonly RoutedUICommand Search = new RoutedUICommand(
+            "Serach Command",
+            "SearchCommand",
+            typeof(RoutedCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.S,ModifierKeys.Shift)
+            });
+    }
+}
